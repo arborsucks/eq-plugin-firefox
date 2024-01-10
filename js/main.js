08cd864ac6f24f9a543dbfe976c582aa1102d63a
2lag <aidenhelfman@gmail.com>
2024-01-10 17:30:00 -0600

    style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
+function setColor( obj ) {
+  var val = $( obj ).val();
+  var min = $( obj ).attr('min');
+  var max = $( obj ).attr('max');
+  var percent = ( val - min ) / ( max - min );
+  var color = 'rgb(' + Math.round( 255 * ( 1 - percent ) ) + ',' + Math.round( 255 * percent ) + ', 0 )';
+  $( obj ).css('background-color', 'rgba( 0, 0, 0, 0 )' );
+
+  var styleSheet = '#'+$( obj ).attr('id') + '::-webkit-slider-thumb { background-color: ' + color + '; } ' +
+                   '#'+$( obj ).attr('id') + '::-moz-range-thumb { background-color: ' + color + '; }';
+
+  $('head').append('<style>' + styleSheet + '</style>');
+}
+
 $('.reset-btn').click( function() {
   var sliderId = $( this ).data('slider');
   $('#' + sliderId).val(0).change();
 });
 
-$('.bar').on('input change', function() {
+$('.reset-all-btn').click( function() {
   $('.bar').each( function() {
-    localStorage.setItem( this.id, $(this).val() );
+    var sliderId = this.id;
+    $('#' + sliderId ).val(0).change();
+  });
+});
+
+$('.bar').on('input change', function() {
+  setColor( this );
+
+  $('.bar').each( function() {
+    localStorage.setItem( this.id, $( this ).val() );
   });
 });
 
@@ -13,6 +36,8 @@ $( document ).ready( function() {
   $('.bar').each( function() {
     var sliderId = this.id;
     var sliderVal = localStorage.getItem( sliderId );
-    $( '#' + sliderId ).val( sliderVal );
+    $('#' + sliderId ).val( sliderVal );
+
+    setColor( this );
   });
 });
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 todo:
- - [ ] figure out how to get javascript to work to reset slider ( its being blocked by the browser )
- - [ ] stylize the sliders
+ - [ ] implement chart.js
  - [ ] research how EQ's work, what math is needed, etc
- - [ ] create function to plot eq curve
  - [ ] implement functionality to actually modify the sound
+ - [ ] create function to plot eq curve
