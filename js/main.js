a1aeacf63d5512c5cae3c1ec4854558a6a6850ce
2lag <aidenhelfman@gmail.com>
2024-01-10 16:30:03 -0600

    done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-$('.reset-btn').click(function() {
-  var sliderId = $(this).data('slider');
+$('.reset-btn').click( function() {
+  var sliderId = $( this ).data('slider');
   $('#' + sliderId).val(0).change();
+});
+
+$('.bar').on('input change', function() {
+  $('.bar').each( function() {
+    localStorage.setItem( this.id, $(this).val() );
+  });
+});
+
+$( document ).ready( function() {
+  $('.bar').each( function() {
+    var sliderId = this.id;
+    var sliderVal = localStorage.getItem( sliderId );
+    $( '#' + sliderId ).val( sliderVal );
+  });
 });
